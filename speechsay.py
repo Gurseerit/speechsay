f049b4d56bfedf94f4217b9fe787ab3ed0f9505f
Gurseerit <48604425+Gurseerit@users.noreply.github.com>
2019-07-18 20:49:11 +0100

    Update speechsay.py

--- speechsay.py
+++ speechsay.py
@@ -1,9 +1,12 @@
-var = input("enter your text: ")
-print("_______")
-print("<" + var + ">")
-print("-------")
-print("    I    ")
-print("  ( ._.)   ")
-print(" /  II    ")
-print(" \___II-")
 
+import sys
+if len(sys.argv) < 2:
+    print("Usage:- Python3 speech.py <text>")
+    sys.exit()
+
+print("   <" + sys.argv[1] + " >")
+print("      |      ")
+print("    (._.)    ")
+print("    / | \    ")
+print("     / \     ")   
+print("Thanks for using speechsay by gurseerit") 
