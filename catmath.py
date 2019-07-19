1a03af09441669e64f7cd3d757bd9546b87c8492
Gurseerit <48604425+Gurseerit@users.noreply.github.com>
2019-07-19 21:10:15 +0100

    created a in development file
    If you want to develop the code
    its a calculator 
    but a cats saying it
    .not meant for cheating on homework 
    .NOT TAKING ANY RESPONSIBILITY IF YOU USE THIS CALCULATOR FOR CHEATING

---
+++ catmath.py
@@ -0,0 +1,8 @@
+num = int(input("number 1: "))
+numb = int(input("number 2: "))
+var = num + numb
+print("______<" + var + ">")
+print("     |    ")
+print("  ( ._.)   ")
+print(" /  II    ")
+print(" \___II-")
