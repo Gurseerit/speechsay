ba31a28b8e57ff53300450da24834093dba213c7
Gurseerit <48604425+Gurseerit@users.noreply.github.com>
2019-07-19 14:17:01 +0100

    created hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+hello = input("enter the text")
+print("<" + hello + ">")
+print("      |      ")
+print("    (._.)    ")
+print("     \\(   )-")
+print("         | | ")
+print("type credits for credits and type version to get version")
+var = input()
+if var == "credits":
+   print("idea from")
+   print("Cowsay")
+   print("Rest of code by")
+   print("Gurseerit")   
+if var == "version":
+     print("speechsay version 3.0 *LATEST*")
+     print("hello.py")
+#end of code
